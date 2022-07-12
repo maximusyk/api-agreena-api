4ce627926484de42701368c34a781dc97529336d
Maksym Usyk <22usyk08@gmail.com>
2022-07-12 20:55:42 +0300

    Final changes

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ module.exports = {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         port: process.env.DATABASE_PORT,
-        host: process.env.DATABASE_HOST,
+        host: process.env.DATABASE_HOST_DOCKER,
         dialect: process.env.DATABASE_DIALECT,
     },
 };
